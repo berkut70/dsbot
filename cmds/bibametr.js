bd87ba64402e9dbb8b169d9a2cdf7cada1aa6e55
berkut70 <51454350+berkut70@users.noreply.github.com>
2019-06-06 20:45:46 +0300

    Update bibametr.js

--- cmds/bibametr.js
+++ cmds/bibametr.js
@@ -2,7 +2,7 @@ const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (dsbot,message,args) => {
     n = Math.floor(Math.random() * (45 - 1 + 1)) + 1;
-    message.channel.send(`У <@${message.author.id}> биба ${n} см! ${<:Pog:537370631461666856>}`);
+    message.channel.send(`У <@${message.author.id}> биба ${n} см! <:Pog:${537370631461666856}>`);
 };
 module.exports.help = {
     name: "bibametr"
