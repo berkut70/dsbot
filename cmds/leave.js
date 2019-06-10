3bf53fb858e8129258aa1637b846b4db816b1858
berkut70 <51454350+berkut70@users.noreply.github.com>
2019-06-10 20:05:23 +0300

    Create leave.js

---
+++ cmds/leave.js
@@ -0,0 +1,8 @@
+const Discord = module.require("discord.js");
+const fs = require("fs");
+module.exports.run = async (dsbot,message,args) => {
+    message.channel.send('хуй соси!');
+};
+module.exports.help = {
+    name: "stop"
+}
